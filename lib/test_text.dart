bde8eb42494ac16a40990223a30a3b691bf45f3e
iloveuhyeon <pyh1111802@naver.com>
2025-01-07 15:54:59 +0900

    test

---
+++ lib/test_text.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PrintText extends StatelessWidget {
+  const PrintText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("TestModule");
+  }
+}
